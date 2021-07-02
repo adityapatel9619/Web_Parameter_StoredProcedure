679352f6ef95813fdab73f1609b9729535393f6e
adityapatel9619 <adityapatel9619@gmail.com>
2021-07-02 13:33:05 +0530

    Add project files.

---
+++ Index.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Web_Parameter_StoredProcedure.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="text-align:center">
+            <h1>Parameterized Stored Procedure</h1>
+            <br />
+            <table style="margin-left:250px;">
+                <tr>
+                    <td>Name</td>
+                    <td>
+                        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Age</td>
+                    <td>
+                        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Country</td>
+                    <td>
+                        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="Button1_Click"/>
+                    </td>
+                    <td>
+                        <asp:Button ID="Button2" runat="server" Text="Select" OnClick="Button2_Click"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="Button3" runat="server" Text="Update" OnClick="Button3_Click"/>
+                    </td>
+                    <td>
+                        <asp:Button ID="Button4" runat="server" Text="Delete" OnClick="Button4_Click"/>
+                    </td>
+                </tr>
+            </table>
+            <br />
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSource1" DataKeyNames="Id">
+                <Columns>
+                    <asp:BoundField  DataField="Id" HeaderText="Id" SortExpression ="Id" InsertVisible="False" ReadOnly="True"/>
+                    <asp:BoundField  DataField="Name" HeaderText="Name" SortExpression ="Name"/>
+                    <asp:BoundField  DataField="Age" HeaderText="Age" SortExpression ="Age"/>
+                    <asp:BoundField DataField="Country" HeaderText="Country" SortExpression="Country" />
+                </Columns>
+            </asp:GridView>
+            
+            <br />
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:SimpleStoredProcConnectionString %>" SelectCommand="SELECT * FROM [Info]"></asp:SqlDataSource>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Web_Parameter_StoredProcedure
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+         protected void Button1_Click(object sender, EventArgs e)
+         {
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-39COJ7F\\SQLEXPRESS;Initial Catalog=SimpleStoredProc;Integrated Security=True");
+            SqlCommand command = new SqlCommand("Proc_ParamWeb", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@Action", "Insert");
+            command.Parameters.AddWithValue("@Name", TextBox1.Text);
+            command.Parameters.AddWithValue("@Age", Convert.ToInt32(TextBox2.Text));
+            command.Parameters.AddWithValue("@Country", TextBox3.Text);
+            connection.Open();
+            command.ExecuteNonQuery();
+            GridView1.Visible = true;
+            GridView1.DataBind();
+            connection.Close();
+         }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-39COJ7F\\SQLEXPRESS;Initial Catalog=SimpleStoredProc;Integrated Security=True");
+            SqlCommand command = new SqlCommand("Proc_ParamWeb", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            connection.Open();
+            command.ExecuteNonQuery();
+            GridView1.Visible = true;
+            GridView1.DataBind();
+            connection.Close();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-39COJ7F\\SQLEXPRESS;Initial Catalog=SimpleStoredProc;Integrated Security=True");
+            SqlCommand command = new SqlCommand("Proc_ParamWeb", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@Action", "Update");
+            command.Parameters.AddWithValue("@Name", TextBox1.Text);
+            command.Parameters.AddWithValue("@Age", Convert.ToInt32(TextBox2.Text));
+            command.Parameters.AddWithValue("@Country", TextBox3.Text);
+            connection.Open();
+            command.ExecuteNonQuery();
+            GridView1.Visible = true;
+            GridView1.DataBind();
+            connection.Close();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-39COJ7F\\SQLEXPRESS;Initial Catalog=SimpleStoredProc;Integrated Security=True");
+            SqlCommand command = new SqlCommand("Proc_ParamWeb", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@Action", "Delete");
+            command.Parameters.AddWithValue("@Name", TextBox1.Text);
+            command.Parameters.AddWithValue("@Age", Convert.ToInt32(TextBox2.Text));
+            command.Parameters.AddWithValue("@Country", TextBox3.Text);
+            connection.Open();
+            command.ExecuteNonQuery();
+            GridView1.Visible = true;
+            GridView1.DataBind();
+            connection.Close();
+        }
+    }
+}
